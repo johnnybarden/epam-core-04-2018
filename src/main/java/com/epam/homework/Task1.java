32485ce965cc3012b1a50685d6a5fbaa679457bb
lostforstupid <lostforstupid@gmail.com>
2018-04-14 01:00:40 +0300

    task 1 is done

--- src/main/java/com/epam/homework/Task1.java
+++ src/main/java/com/epam/homework/Task1.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task1 {
 
     /**
@@ -33,10 +35,44 @@ public class Task1 {
      * MIN (29): Унылая пора! Очей очарованье!
      * MAX (35): Приятна мне твоя прощальная краса —
      */
-    public static void main(String[] args) {
-        // TODO реализация
 
-        // TODO System.out.println("MIN (" + minLength + "): " + minString);
-        // TODO System.out.println("MAX (" + maxLength + "): " + maxString);
+    public static void main(String[] args) {
+        int N;
+        int minLength;
+        int maxLength;
+        String minString;
+        String maxString;
+        String inString;
+
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        in.nextLine();
+
+        if (N <= 0 || N >= 100) {
+            throw new IllegalArgumentException("N should be more than 0 and less than 100");
+        }
+
+        minString = in.nextLine();
+        minLength = minString.length();
+
+        maxString = minString;
+        maxLength = minLength;
+
+        for (int i = 1; i < N; i++) {
+            inString = in.nextLine();
+
+            if (inString.length() <= minLength) {
+                minString = inString;
+                minLength = minString.length();
+            }
+
+            if (inString.length() >= maxLength) {
+                maxString = inString;
+                maxLength = maxString.length();
+            }
+        }
+
+        System.out.println("MIN (" + minLength + "): " + minString);
+        System.out.println("MAX (" + maxLength + "): " + maxString);
     }
 }
