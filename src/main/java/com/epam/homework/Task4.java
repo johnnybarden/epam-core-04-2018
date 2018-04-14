1aa4202e14f0221d780e6a380ca03ed055f30d6b
lostforstupid <lostforstupid@gmail.com>
2018-04-14 20:51:43 +0300

    task 4 is done

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task4 {
 
     /**
@@ -25,9 +27,63 @@ public class Task4 {
      * Выходные данные:
      * a
      */
-    public static void main(String[] args) {
-        // TODO реализация
 
-        // TODO System.out.println(wordWithMinimalNumDiffLetters);
+    private static int countNumDiffLetters (String word) {
+        char[] allLettersInWord = word.toCharArray();
+        char[] uniqueLettersInWord = new char[word.length()];
+        boolean isUnique;
+
+        uniqueLettersInWord[0] = allLettersInWord[0];
+
+        int index = 1;
+        int j;
+
+        for (int i = 1; i < allLettersInWord.length; i++) {
+            isUnique = true;
+            j = 0;
+
+            while (j < uniqueLettersInWord.length && isUnique && uniqueLettersInWord[j] != '\u0000') {
+                if (allLettersInWord[i] == uniqueLettersInWord[j]) {
+                    isUnique = false;
+                }
+
+                j++;
+            }
+
+            if (isUnique) {
+                uniqueLettersInWord[index++] = allLettersInWord[i];
+            }
+        }
+
+        System.out.println(index);
+        return index ;
+    }
+
+    public static void main(String[] args) {
+        int N;
+        String wordWithMinimalNumDiffLetters;
+        int minimalNumDiffLetters;
+
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        in.nextLine();
+
+        wordWithMinimalNumDiffLetters = in.next();
+        minimalNumDiffLetters = countNumDiffLetters(wordWithMinimalNumDiffLetters);
+
+        String tempWord;
+        int numOfDiffLetters;
+
+        for (int i = 1; i < N; i++) {
+            tempWord = in.next();
+            numOfDiffLetters = countNumDiffLetters(tempWord);
+
+            if (numOfDiffLetters < minimalNumDiffLetters) {
+                wordWithMinimalNumDiffLetters = tempWord;
+                minimalNumDiffLetters = numOfDiffLetters;
+            }
+        }
+
+        System.out.println(wordWithMinimalNumDiffLetters);
     }
 }
