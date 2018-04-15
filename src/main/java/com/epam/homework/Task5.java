3959fb5b3d13e97bb2720f01b8b4d55a6836c077
Ilya21 <vertileckii.ilya@gmail.com>
2018-04-16 02:57:32 +0300

    Solution of task5

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -1,32 +1,66 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task5 {
 
     /**
      * Ввести N слов с консоли.
      * Найти количество слов, содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество слов в строке
      * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено количество слов, состоящих только из символов латинского алфавита и содержащих равное количество гласных и согласных букв.
      * Количество различных вхождений одной буквы в слове учитывается.
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * Язык программирования Java is widespread
-     *
+     * <p>
      * Выходные данные:
      * 2
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        in.nextLine();
 
-        // TODO System.out.println(countWordsWithSameNumVowelsAndConsonants);
+        String inputString = in.nextLine();
+        System.out.println(getCountWordsWithSameNumVowelsAndConsonants(inputString));
+    }
+
+    private static Integer getCountWordsWithSameNumVowelsAndConsonants(String inputString) {
+        String[] wordsArr = inputString.split(" ");
+        Integer countWordsWithSameNumVowelsAndConsonants = 0;
+
+        nextWord:
+        for (String currentWord : wordsArr) {
+            StringBuilder word = new StringBuilder(currentWord);
+            Integer countVowel = 0;
+            Integer countConsonant = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                if (!((word.charAt(i) >= 65 && word.charAt(i) <= 90)
+                        || ((word.charAt(i) >= 97 && word.charAt(i) <= 122)))) {
+                    continue nextWord;
+                } else {
+                    if (("" + word.charAt(i)).matches("^[aeiouAEIOU]$")) {
+                        countVowel++;
+                    } else {
+                        countConsonant++;
+                    }
+                }
+            }
+
+            if (countConsonant.equals(countVowel))
+                countWordsWithSameNumVowelsAndConsonants++;
+        }
+        return countWordsWithSameNumVowelsAndConsonants;
     }
 }
