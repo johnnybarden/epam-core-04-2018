54be634d64a3edd186bcdd04d64aba4624d378c4
LicenserX <Licesnent@gmail.com>
2018-04-15 20:44:08 +0300

    Первая таска, не очень изящно, но вроде работает.

--- src/main/java/com/epam/homework/Task1.java
+++ src/main/java/com/epam/homework/Task1.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task1 {
 
     /**
@@ -7,36 +11,64 @@ public class Task1 {
      * Найти среди них самую короткую и самую длинную.
      * Вывести найденные строки и их длину.
      * Если строк, удовлетворяющих условию, более одной - вывести последнюю из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * MIN ($длина_минимальной_строки$): $минимальная_строка$
      * MAX ($длина_максимальной_строки$): $максимальная_строка$
-     *
+     * <p>
      * -----------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Унылая пора! Очей очарованье!
      * Приятна мне твоя прощальная краса —
      * Люблю я пышное природы увяданье,
      * В багрец и в золото одетые леса,
-     *
+     * <p>
      * Выходные данные:
      * MIN (29): Унылая пора! Очей очарованье!
      * MAX (35): Приятна мне твоя прощальная краса —
      */
     public static void main(String[] args) {
-        // TODO реализация
 
-        // TODO System.out.println("MIN (" + minLength + "): " + minString);
-        // TODO System.out.println("MAX (" + maxLength + "): " + maxString);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        int count = 0;
+        int minLength = 0;
+        int maxLength = 0;
+        String maxString = null;
+        String minString = null;
+
+        try {
+            int a = Integer.valueOf(br.readLine());
+            while (count < a && a < 100 && a > 1) {
+                line = br.readLine();
+                while (line.length() > maxLength) {
+                    maxString = line;
+                    maxLength = line.length();
+                }
+                if (minLength == 0) {
+                    minLength = line.length();
+                    minString = line;
+                }
+                while (line.length() < minLength) {
+                    minString = line;
+                    minLength = line.length();
+                }
+                count++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("MIN (" + minLength + "): " + minString);
+        System.out.println("MAX (" + maxLength + "): " + maxString);
     }
 }
