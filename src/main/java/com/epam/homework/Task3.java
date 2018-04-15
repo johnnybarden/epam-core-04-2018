e1f4383318935d6e2fe00b44b9d96224c5cd0fe5
Ilya21 <vertileckii.ilya@gmail.com>
2018-04-16 00:45:43 +0300

    Solution of task3

--- src/main/java/com/epam/homework/Task3.java
+++ src/main/java/com/epam/homework/Task3.java
@@ -1,28 +1,30 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task3 {
 
     /**
      * Ввести N строк с консоли.
      * Вывести те строки, длина которых меньше средней.
      * Под 'средней' подразумевается среднеарифметическая величина длины всех строк, округленная до целого в меньшую сторону.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество доступных для чтения строк
      * N строк
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должна быть выведена средняя длина (округленная до целого в меньшую сторону) и строки, удовлетворяющие условию.
      * Порядок появления строк в выходном потоке должен совпадать с порядком их появления во входном.
-     *
+     * <p>
      * AVERAGE (средняя длина)
      * (длина строки): строка с длиной меньше средней
      * ...
      * (длина строки): строка с длиной меньше средней
-     *
+     * <p>
      * ------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * Послушайте!
@@ -30,18 +32,31 @@ public class Task3 {
      * Значит - это кому-нибудь нужно?
      * Значит - кто-то хочет, чтобы они были?
      * Значит - кто-то называет эти плевочки жемчужиной?
-     *
+     * <p>
      * Выходные данные:
      * AVERAGE (31)
      * (11): Послушайте!
      * (28): Ведь, если звезды зажигают -
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
 
-        // TODO System.out.println("AVERAGE (" + averageLength + ")");
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        Integer N = in.nextInt();
+        in.nextLine();
+
+        Integer averageLength = 0;
+        String[] result = new String[N];
+        for (int i = 0; i < N; i++) {
+            result[i] = in.nextLine();
+            averageLength += result[i].length();
+        }
+
+        averageLength = averageLength / N;
+
+        System.out.println("AVERAGE (" + averageLength + ")");
+        for (String current : result) {
+            if (current.length() < averageLength)
+                System.out.println("(" + current.length() + "): " + current);
+        }
     }
 }
