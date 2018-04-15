486ca8d36845f0918010ef58bd97a497ee8fc4ef
Ilya21 <vertileckii.ilya@gmail.com>
2018-04-16 02:03:53 +0300

    Solution of task4

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task4 {
 
     /**
@@ -7,27 +9,55 @@ public class Task4 {
      * Найти слово, в котором число различных символов минимально.
      * Символы верхнего и нижнего регистра считать различными.
      * Если таких слов несколько, найти первое из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество слов в строке
      * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено слово, содержащее наименьшее число различных символов.
-     *
+     * <p>
      * -------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Cake is a lie
-     *
+     * <p>
      * Выходные данные:
      * a
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        in.nextLine();
 
-        // TODO System.out.println(wordWithMinimalNumDiffLetters);
+        String inputString = in.nextLine();
+
+        System.out.println(getWordWithMinUniqueLetters(inputString));
+    }
+
+    private static String getWordWithMinUniqueLetters(String inputString) {
+
+        String wordWithMinimalNumDiffLetters = "";
+        String[] wordsArr = inputString.split(" ");
+        Integer countMinUniqueChar = Integer.MAX_VALUE;
+
+        for (int i = 0; i < wordsArr.length; i++) {
+            StringBuilder word = new StringBuilder(wordsArr[i]);
+            StringBuffer uniqueChar = new StringBuffer();
+
+            for (int j = 0; j < wordsArr[i].length(); j++) {
+                if (!uniqueChar.toString().contains("" + word.charAt(j))) {
+                    uniqueChar.append(word.charAt(j));
+                }
+            }
+
+            if (countMinUniqueChar > uniqueChar.length()) {
+                countMinUniqueChar = uniqueChar.length();
+                wordWithMinimalNumDiffLetters = word.toString();
+            }
+        }
+        return wordWithMinimalNumDiffLetters;
     }
 }
