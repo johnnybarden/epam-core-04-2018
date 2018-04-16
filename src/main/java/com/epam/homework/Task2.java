836edf89c9d398d07f08b68b4e7d80646eae696e
MalyshI <r_alliance@mail.ru>
2018-04-16 16:56:24 +0300

    TreeSort without alphabetical order

--- src/main/java/com/epam/homework/Task2.java
+++ src/main/java/com/epam/homework/Task2.java
@@ -1,19 +1,22 @@
 package com.epam.homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task2 {
 
     /**
      * Ввести N строк с консоли.
      * Упорядочить и вывести строки в порядке возрастания значений их длины.
      * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должны быть выведены N строк следующего вида:
      * (длина строки): строка наименьшей длины
@@ -21,17 +24,17 @@ public class Task2 {
      * (длина строки): строка большей длины
      * ...
      * (длина строки): строка наибольшей длины
-     *
+     * <p>
      * ----------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Тихо струится река серебристая
      * В царстве вечернем зеленой весны.
      * Солнце садится за горы лесистые.
      * Рог золотой выплывает луны.
-     *
+     * <p>
      * Выходные данные:
      * (27): Рог золотой выплывает луны.
      * (30): Тихо струится река серебристая
@@ -39,10 +42,59 @@ public class Task2 {
      * (33): В царстве вечернем зеленой весны.
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+        //String[] strings = new String[size];
+        Tree tree = new Tree(scanner.nextLine());
+        for (int i = 0; i < size; i++) {
+            tree.insert(new Tree(scanner.nextLine()));
+        }
 
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        tree.traverse();
+        /*for (String current : tree) {
+            System.out.println("(" + current.length() + "): " + current);
+        }*/
+    }
+}
+
+    class Tree {
+        public Tree left;            // левое и правое поддеревья и ключ
+        public Tree right;
+        public String key;
+        public Tree(){
+
+        }
+
+        public Tree(String k) {        // конструктор с инициализацией ключа
+            key = k;
+        }
+
+        /*  insert (добавление нового поддерева (ключа))
+            сравнить ключ добавляемого поддерева (К) с ключом корневого узла (X).
+            Если K>=X, рекурсивно добавить новое дерево в правое поддерево.
+            Если K<X, рекурсивно добавить новое дерево в левое поддерево.
+            Если поддерева нет, то вставить на это место новое дерево
+        */
+   public void insert(Tree aTree) {
+        if (aTree.key.length() < key.length())
+            if (left != null) left.insert(aTree);
+            else left = aTree;
+        else if (right != null) right.insert(aTree);
+        else right = aTree;
+    }
+
+    /*  traverse (обход)
+        Рекурсивно обойти левое поддерево.
+        Применить функцию f (печать) к корневому узлу.
+        Рекурсивно обойти правое поддерево.
+    */
+    public void traverse() {
+        if (left != null)
+            left.traverse();
+
+        System.out.println(" " + this.key);
+
+        if (right != null)
+            right.traverse();
     }
 }
