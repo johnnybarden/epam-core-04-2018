6816dd688df0f08d7c6dc74fc2aab3ddf87e506c
Alina Putintseva <aaputintseva@niuitmo.ru>
2018-04-16 15:33:22 +0300

    Task 2 was added

--- src/main/java/com/epam/homework/Task2.java
+++ src/main/java/com/epam/homework/Task2.java
@@ -1,19 +1,23 @@
 package com.epam.homework;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Task2 {
 
     /**
      * Ввести N строк с консоли.
      * Упорядочить и вывести строки в порядке возрастания значений их длины.
      * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должны быть выведены N строк следующего вида:
      * (длина строки): строка наименьшей длины
@@ -21,17 +25,17 @@ public class Task2 {
      * (длина строки): строка большей длины
      * ...
      * (длина строки): строка наибольшей длины
-     *
+     * <p>
      * ----------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Тихо струится река серебристая
      * В царстве вечернем зеленой весны.
      * Солнце садится за горы лесистые.
      * Рог золотой выплывает луны.
-     *
+     * <p>
      * Выходные данные:
      * (27): Рог золотой выплывает луны.
      * (30): Тихо струится река серебристая
@@ -39,10 +43,39 @@ public class Task2 {
      * (33): В царстве вечернем зеленой весны.
      */
     public static void main(String[] args) {
-        // TODO реализация
 
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        ArrayList<String> unsorted;
+
+        try (Scanner in = new Scanner(System.in)) {
+
+            int count = Integer.valueOf(in.nextLine());
+            unsorted = new ArrayList<>(count);
+
+            for (int i = 0; i < count; i++) {
+                unsorted.add(in.nextLine());
+            }
+        }
+
+        // Step 1. Alphabetizing
+        Collections.sort(unsorted);
+
+        // Step 2. Sort by string length
+
+        ArrayList<String> sorted = new ArrayList<>();
+
+        while (!unsorted.isEmpty()) {
+            String minString = unsorted.get(0);
+            for (String string : unsorted) {
+                if (string.length() < minString.length()) {
+                    minString = string;
+                }
+            }
+            sorted.add(minString);
+            unsorted.remove(minString);
+        }
+
+        for (String str : sorted) {
+            System.out.println("(" + str.length() + "): " + str);
+        }
     }
 }
