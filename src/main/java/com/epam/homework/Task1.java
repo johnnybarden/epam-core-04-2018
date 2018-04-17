1dd43b09b6b78ec3b22244d2679bbebc688f0ee1
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-04-17 03:00:01 +0300

    first commit

--- src/main/java/com/epam/homework/Task1.java
+++ src/main/java/com/epam/homework/Task1.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task1 {
 
     /**
@@ -34,9 +36,27 @@ public class Task1 {
      * MAX (35): Приятна мне твоя прощальная краса —
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner sc = new Scanner(System.in);
 
-        // TODO System.out.println("MIN (" + minLength + "): " + minString);
-        // TODO System.out.println("MAX (" + maxLength + "): " + maxString);
+        int lineCount = Integer.parseInt(sc.nextLine());
+        
+        String minString = sc.nextLine();
+        String maxString = minString;
+        
+        String nextLine;
+
+        while(--lineCount > 0) {
+            nextLine = sc.nextLine();
+
+            if (nextLine.length() <= minString.length()) {
+                minString = nextLine;
+            }
+            if (nextLine.length() >= maxString.length()) {
+                maxString = nextLine;
+            }
+        }
+
+        System.out.println("MIN (" + minString.length() + "): " + minString);
+        System.out.println("MAX (" + maxString.length() + "): " + maxString);
     }
 }
