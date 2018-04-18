2d09dace26d32521e62af61ea8fc55a9078c5e8a
Nikolay Imbirev <imbirev.nikolay@gmail.com>
2018-04-18 20:29:47 +0300

    5 task

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -1,32 +1,46 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task5 {
 
-    /**
-     * Ввести N слов с консоли.
-     * Найти количество слов, содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.
-     *
-     * Формат входных данных:
-     * N (целое число) - количество слов в строке
-     * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
-     * Формат выходных данных:
-     * В результате выполнения в выходной поток должно быть выведено количество слов, состоящих только из символов латинского алфавита и содержащих равное количество гласных и согласных букв.
-     * Количество различных вхождений одной буквы в слове учитывается.
-     *
-     * ---------------------------------------------------------------------------------------------------
-     * Пример выполнения задания:
-     *
-     * Входные данные:
-     * 5
-     * Язык программирования Java is widespread
-     *
-     * Выходные данные:
-     * 2
-     */
-    public static void main(String[] args) {
-        // TODO реализация
+    private static List<String> listOfWords;
 
-        // TODO System.out.println(countWordsWithSameNumVowelsAndConsonants);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = Integer.valueOf(scanner.nextLine());
+        int count = 0;
+        int glassCounter = 0;
+        int soglassCounter = 0;
+        boolean flag = false;
+        String input = scanner.nextLine();
+        listOfWords = new ArrayList<>(Arrays.asList(input.split(" ")));
+        for (String a : listOfWords) {
+            for (int i = 0; i < a.length(); i++) {
+                char c = a.charAt(i);
+                if (((c >= 'a')&&(c <= 'z')) || ((c >= 'A')&&(c <= 'Z'))) {
+                    flag = true;
+                    c = Character.toLowerCase(c);
+                    switch (c) {
+                        case 'a':
+                        case 'e':
+                        case 'i':
+                        case 'o':
+                        case 'u':
+                        case 'y': glassCounter++;
+                            break;
+                        default: soglassCounter++;
+                    }
+                } else {
+                    flag = false;
+                }
+            }
+            if (flag && glassCounter == soglassCounter) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
+
 }
