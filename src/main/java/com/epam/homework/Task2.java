d6f42262054952d84697d70f9cf7a9dc1c6dfd44
LicenserX <your@mail.here>
2018-04-18 21:18:16 +0300

    Вторая таска получилась немного изящней.

--- src/main/java/com/epam/homework/Task2.java
+++ src/main/java/com/epam/homework/Task2.java
@@ -1,19 +1,26 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Task2 {
 
     /**
      * Ввести N строк с консоли.
      * Упорядочить и вывести строки в порядке возрастания значений их длины.
      * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должны быть выведены N строк следующего вида:
      * (длина строки): строка наименьшей длины
@@ -21,17 +28,17 @@ public class Task2 {
      * (длина строки): строка большей длины
      * ...
      * (длина строки): строка наибольшей длины
-     *
+     * <p>
      * ----------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Тихо струится река серебристая
      * В царстве вечернем зеленой весны.
      * Солнце садится за горы лесистые.
      * Рог золотой выплывает луны.
-     *
+     * <p>
      * Выходные данные:
      * (27): Рог золотой выплывает луны.
      * (30): Тихо струится река серебристая
@@ -39,10 +46,24 @@ public class Task2 {
      * (33): В царстве вечернем зеленой весны.
      */
     public static void main(String[] args) {
-        // TODO реализация
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> stringList = new ArrayList<>();
 
-        // TODO foreach($current : $result) {
-        // TODO     System.out.println("(" + current.length() + "): " + current);
-        // TODO }
+        try {
+            int numberSentences = Integer.valueOf(reader.readLine());
+            while (numberSentences-- > 0) {
+                stringList.add(reader.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Collections.sort(stringList, (o1, o2) ->
+                (o1.length() - o2.length() == 0 ? o1.compareTo(o2) : o1.length() - o2.length()));
+
+        for (String current : stringList) {
+            System.out.println("(" + current.length() + "): " + current);
+        }
     }
 }
+
