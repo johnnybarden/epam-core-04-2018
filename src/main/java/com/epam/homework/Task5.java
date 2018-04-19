609fb895793df5d38522e718992592ab0c8165a1
goponenkoev <goponenko.yeakaterina@yandex.ru>
2018-04-20 01:24:38 +0300

    Removed checker for latin letters

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -38,7 +38,7 @@ public class Task5 {
             String temp = scanner.next();
             numVowels = 0;
             numConsonants = 0;
-            if ( temp.matches( "^[a-zA-Z0-9]+$" ) && (temp.length() % 2 == 0) ) {
+            if ( temp.length() % 2 == 0 ) {
                 for (int j = 0; j < temp.length(); j++) {
                     if ( Character.toString( temp.charAt( j ) ).matches( "(?i:[aeiouy]).*" ) ) {
                         numConsonants++;
