7112c8e52c094826c0d2619dcb7be4d469dafee5
MalyshI <r_alliance@mail.ru>
2018-04-19 03:23:06 +0300

    First version of programm. @Ilia Malyshev

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -1,32 +1,78 @@
 package com.epam.homework;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Task5 {
 
     /**
      * Ввести N слов с консоли.
      * Найти количество слов, содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество слов в строке
      * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено количество слов, состоящих только из символов латинского алфавита и содержащих равное количество гласных и согласных букв.
      * Количество различных вхождений одной буквы в слове учитывается.
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * Язык программирования Java is widespread
-     *
+     * <p>
      * Выходные данные:
      * 2
      */
-    public static void main(String[] args) {
-        // TODO реализация
 
-        // TODO System.out.println(countWordsWithSameNumVowelsAndConsonants);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.next());
+
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = scanner.next().toLowerCase();
+        }
+
+        System.out.println(CountWordsWithSameNumVowelsAndConsonants(strings));
+    }
+
+
+    static int CountWordsWithSameNumVowelsAndConsonants(String[] strings) {
+
+        HashSet<Character> consonants = new HashSet<>();
+        Collections.addAll(consonants, 'a', 'e', 'i', 'o', 'u', 'y');
+        HashSet<Character> vowels = new HashSet<>();
+        Collections.addAll(vowels, 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+
+        int countWordsWithSameNumVowelsAndConsonants = 0;
+
+        OUTER:
+        for (String str : strings) {
+            int numberVowels = 0;
+            int numberConsonants = 0;
+
+            if (str.length() % 2 == 0) {
+                for (int i = 0; i < str.length(); i++) {
+                    if (consonants.contains(str.charAt(i))) {
+                        numberConsonants += 1;
+                    } else if (vowels.contains(str.charAt(i))) {
+                        numberVowels += 1;
+                    } else {
+                        continue OUTER;
+                    }
+                }
+                if (numberConsonants == numberVowels) {
+                    countWordsWithSameNumVowelsAndConsonants += 1;
+                }
+            }
+        }
+        return countWordsWithSameNumVowelsAndConsonants;
     }
 }
+
+
