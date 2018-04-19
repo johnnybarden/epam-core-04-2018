ece1de06fad4c21dcbbe9a1d6e4a3f0f3b3a2d0a
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-04-19 19:56:17 +0300

    First commit

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task4 {
 
     /**
@@ -26,8 +28,26 @@ public class Task4 {
      * a
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner sc = new Scanner(System.in)) {
+            int wordsCount = Integer.valueOf(sc.nextLine());
 
-        // TODO System.out.println(wordWithMinimalNumDiffLetters);
+            String wordWithMinimalNumDiffLetters = sc.next();
+            int numDiffLetters = wordWithMinimalNumDiffLetters.length();
+
+            for (int i = 0; i < wordsCount - 1; i++) {
+                String word = sc.next();
+
+                Set<Character> uniqueLetters = new HashSet<>();
+                for (char letter: word.toCharArray()) {
+                    uniqueLetters.add(letter);
+                }
+
+                if (uniqueLetters.size() < numDiffLetters) {
+                    numDiffLetters = uniqueLetters.size();
+                    wordWithMinimalNumDiffLetters = word;
+                }
+            }
+            System.out.println(wordWithMinimalNumDiffLetters);
+        }
     }
 }
