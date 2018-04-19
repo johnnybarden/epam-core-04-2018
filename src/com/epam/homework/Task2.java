cfadfb37a2a6cb399c841ffe065475961dd2a8b0
goponenkoev <goponenko.yeakaterina@yandex.ru>
2018-04-19 12:33:31 +0300

    Completed

--- src/com/epam/homework/Task2.java
+++ src/com/epam/homework/Task2.java
@@ -1,25 +1,59 @@
 package com.epam.homework;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Task2 {
 
+    /**
+     * Ввести N строк с консоли.
+     * Упорядочить и вывести строки в порядке возрастания значений их длины.
+     * В случае, если длины строк совпадают - упорядочить их в лексикографическом порядке.
+     * <p>
+     * Формат входных данных:
+     * N (целое число, 0 < N < 100) - количество доступных для чтения строк
+     * Строка_1
+     * Строка_2
+     * ...
+     * Строка_N
+     * <p>
+     * Формат выходных данных:
+     * В результате выполнения в выходной поток должны быть выведены N строк следующего вида:
+     * (длина строки): строка наименьшей длины
+     * (длина строки): строка большей длины
+     * (длина строки): строка большей длины
+     * ...
+     * (длина строки): строка наибольшей длины
+     * <p>
+     * ----------------------------------------------------------------------------------------------
+     * Пример выполнения задания:
+     * <p>
+     * Входные данные:
+     * 4
+     * Тихо струится река серебристая
+     * В царстве вечернем зеленой весны.
+     * Солнце садится за горы лесистые.
+     * Рог золотой выплывает луны.
+     * <p>
+     * Выходные данные:
+     * (27): Рог золотой выплывает луны.
+     * (30): Тихо струится река серебристая
+     * (32): Солнце садится за горы лесистые.
+     * (33): В царстве вечернем зеленой весны.
+     */
     public static void main(String[] args) {
-        String[] strings;
-        try (Scanner scanner = new Scanner( System.in )) {
-            int number = Integer.valueOf( scanner.nextLine() );
-            strings = new String[number];
-
+        Scanner scan = new Scanner( System.in );
+        if ( scan.hasNextInt() ) {
+            int number = Integer.valueOf( scan.nextLine() );
+            List<String> myList = new ArrayList<>(number);
             for (int i = 0; i < number; i++) {
-                strings[i] = scanner.nextLine();
+                myList.add( scan.nextLine() );
             }
-        }
-
-        Arrays.sort( strings, Task2::compare );
-
-        for (String current : strings) {
-            System.out.println( "(" + current.length() + "): " + current );
+            myList.sort( Task2::compare );
+            for (String cur : myList) {
+                System.out.println( "(" + cur.length() + "): " + cur );
+            }
         }
     }
 
