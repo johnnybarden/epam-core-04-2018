a3894a56e1c1be175b3e00d4df9cfae76aeae1e8
Alina Putintseva <putintseva.alina@gmail.com>
2018-04-20 21:53:25 +0300

    New method was added

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -1,7 +1,8 @@
 package com.epam.homework;
 
-import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Task7 {
 
@@ -10,23 +11,23 @@ public class Task7 {
      * Найти слова, состоящие только из различных символов английского алфавита.
      * Символы верхнего и нижнего регистра считать одинаковыми.
      * В случае, если слово встречается более одного раза - вывести его единожды.
-     * 
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
-     * Строка, содержащая нимимум N слов (состоящих только из букв английского языка), разделенных пробелами
-     * 
+     * Строка, содержащая минимум N слов (состоящих только из букв английского языка), разделенных пробелами
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должна быть выведена строка, содержащая найденные слова, разделенные пробелами.
      * Порядок слов должен совпадать с порядком их появления во входной последовательности.
      * В случае, если не найдено ни одного слова, удовлетворяющего условию - в поток должно быть выведено "NOT FOUND".
-     * 
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     * 
+     * <p>
      * Входные данные:
      * 11
      * The Java programming language is a general-purpose, concurrent, class-based, object-oriented language
-     * 
+     * <p>
      * Выходные данные:
      * The is a
      */
@@ -34,15 +35,13 @@ public class Task7 {
         try (Scanner in = new Scanner(System.in)) {
 
             int wordCount = Integer.valueOf(in.next());
-            ArrayList<String> result = new ArrayList<>();
+            Set<String> result = new LinkedHashSet<>();
 
             for (int i = 0; i < wordCount; i++) {
 
                 String word = in.next();
-                if (word.toLowerCase().chars().distinct().count() == word.chars().count()) {
-                    if (!result.contains(word)) {
-                        result.add(word);
-                    }
+                if (containsOnlyUniqueChars(word)) {
+                    result.add(word);
                 }
             }
 
@@ -50,11 +49,20 @@ public class Task7 {
                 System.out.println("NOT FOUND");
             } else {
                 StringBuilder sb = new StringBuilder();
+
                 for (String word : result) {
-                    sb.append(word).append(' ');
+                    if (sb.length() != 0) {
+                        sb.append(' ');
+                    }
+                    sb.append(word);
                 }
+
                 System.out.println(sb);
             }
         }
+    }
+
+    private static boolean containsOnlyUniqueChars(String word) {
+        return word.toLowerCase().chars().distinct().count() == word.length();
     }
 }
