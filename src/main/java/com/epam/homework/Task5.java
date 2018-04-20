ce7209330294eaeaec8b2ba8874bc43b357efcbb
LicenserX <your@mail.here>
2018-04-20 22:20:25 +0300

    Страшная снаружи, добрая внутри - Вот такая тасочка номера пяти)

--- src/main/java/com/epam/homework/Task5.java
+++ src/main/java/com/epam/homework/Task5.java
@@ -1,32 +1,49 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task5 {
 
     /**
      * Ввести N слов с консоли.
      * Найти количество слов, содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество слов в строке
      * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено количество слов, состоящих только из символов латинского алфавита и содержащих равное количество гласных и согласных букв.
      * Количество различных вхождений одной буквы в слове учитывается.
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * Язык программирования Java is widespread
-     *
+     * <p>
      * Выходные данные:
      * 2
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int numberWords = scanner.nextInt();
+        String currentWord;
+        String subString;
+        int difference;
+        int countWordsWithSameNumVowelsAndConsonants = 0;
 
-        // TODO System.out.println(countWordsWithSameNumVowelsAndConsonants);
+        while (numberWords-- > 0) {
+            currentWord = scanner.next();
+            if (currentWord.matches("[a-zA-Z]+")) {
+                subString = currentWord.replaceAll("(?i:[aeiouy])", "");
+                difference = currentWord.length() - subString.length();
+                if (difference == subString.length()) {
+                    countWordsWithSameNumVowelsAndConsonants = countWordsWithSameNumVowelsAndConsonants + 1;
+                }
+            }
+        }
+        System.out.println(countWordsWithSameNumVowelsAndConsonants);
     }
 }
