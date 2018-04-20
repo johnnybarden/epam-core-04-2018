4e0570f5e1b5a35e546c274b2973e0aa872aa8f5
Alina Putintseva <aaputintseva@niuitmo.ru>
2018-04-20 15:10:21 +0300

    Revert "Refactored with Arrays.sort()"
    This reverts commit 577e1e1

--- src/main/java/com/epam/homework/Task6.java
+++ src/main/java/com/epam/homework/Task6.java
@@ -1,6 +1,5 @@
 package com.epam.homework;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task6 {
@@ -52,10 +51,7 @@ public class Task6 {
                     continue;
                 }
 
-                char[] sorted = word.toCharArray();
-                Arrays.sort(sorted);
-
-                if (String.valueOf(sorted).equals(word)) {
+                if (isSortedByCharCodes(word)) {
                     System.out.println(word);
                     return;
                 }
@@ -63,5 +59,14 @@ public class Task6 {
         }
 
         System.out.println("NOT FOUND");
+    }
+
+    private static boolean isSortedByCharCodes(String word) {
+        for (int i = 0; i < word.length() - 1; i++) {
+            if (word.charAt(i) > word.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
