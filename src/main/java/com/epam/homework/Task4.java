92caa321696f2fad9b22f405c7d8aeb6fe9d60c0
heydancer <Vision3415841>
2018-04-20 11:38:06 +0300

    Task4 completed

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,5 +1,10 @@
 package com.epam.homework;
 
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.HashSet;
+
 public class Task4 {
 
     /**
@@ -7,27 +12,41 @@ public class Task4 {
      * Найти слово, в котором число различных символов минимально.
      * Символы верхнего и нижнего регистра считать различными.
      * Если таких слов несколько, найти первое из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число) - количество слов в строке
      * Строка, содержащая указанное количество слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено слово, содержащее наименьшее число различных символов.
-     *
+     * <p>`
      * -------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Cake is a lie
-     *
+     * <p>
      * Выходные данные:
      * a
      */
-    public static void main(String[] args) {
-        // TODO реализация
-
-        // TODO System.out.println(wordWithMinimalNumDiffLetters);
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = Integer.parseInt(scanner.next());
+        int counter = Integer.MAX_VALUE;
+        String wordWithMinimalNumDiffLetters = "";
+        HashSet<Character> characters;
+        for (int i = 0; i < N; i++) {
+            String string = scanner.next();
+            characters = new HashSet<>();
+            for (int j = 0; j < string.length(); j++) {
+                characters.add(string.charAt(j));
+            }
+            if (characters.size() < counter) {
+                counter = characters.size();
+                wordWithMinimalNumDiffLetters = string;
+            }
+        }
+        System.out.println(wordWithMinimalNumDiffLetters);
     }
 }
