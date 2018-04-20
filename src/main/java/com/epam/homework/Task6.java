1301260885c9193140ad39df93eb1fc3c81a59a5
MalyshI <r_alliance@mail.ru>
2018-04-20 14:56:59 +0300

    First solution for task6. @Ilia Malyshev

--- src/main/java/com/epam/homework/Task6.java
+++ src/main/java/com/epam/homework/Task6.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task6 {
 
     /**
@@ -7,35 +9,62 @@ public class Task6 {
      * Найти слово, буквы в котором идут в строгом порядке возрастания их кодов: word.charAt(i) < word.charAt(i + 1).
      * Если таких слов несколько, найти первое из них.
      * Слова состоящие из одного символа не учитывать.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество слов в строке
      * Строка, содержащая нимимум N слов, разделенных пробелами
-     *
+     * <p>
      * Формат выходных данных:
      * В результате выполнения в выходной поток должно быть выведено слово, удовлетворяющее условию.
      * Если подходящее слово не найдено в выходной поток выводится строка "NOT FOUND"
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 12
      * The original and reference implementation Java compilers were originally released by Sun
-     *
+     * <p>
      * Выходные данные:
      * by
-     *
+     * <p>
      * ===================================================================================================
-     *
+     * <p>
      * Входные данные:
      * 4
      * Кто-то позвонил в дверь
-     *
+     * <p>
      * Выходные данные:
      * NOT FOUND
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.next());
+        String word;
+        boolean isFirstWordFound = false;
+        int counter = 0;
+
+        do {
+            word = scanner.next();
+            if (word.length() > 1) {
+                isFirstWordFound = isWordWithCharactersInOrder(word);
+            }
+        } while (!(isFirstWordFound) && (++counter < n));
+
+        if (isFirstWordFound) {
+            System.out.println(word);
+        } else System.out.println("NOT FOUND");
+
+    }
+
+    static boolean isWordWithCharactersInOrder(String string) {
+        boolean charsInOrder = true;
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i - 1) >= string.charAt(i)) {
+                charsInOrder = false;
+                break;
+            }
+        }
+        return charsInOrder;
     }
 }
