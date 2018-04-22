2b0aa8a4f31e8212e490e4bf63bc1682b641bd42
akalji <akalji@ya.ru>
2018-04-22 22:07:24 +0300

    Solved

--- src/main/java/com/epam/homework/Task4.java
+++ src/main/java/com/epam/homework/Task4.java
@@ -1,4 +1,5 @@
 package com.epam.homework;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Task4 {
@@ -32,8 +33,15 @@ public class Task4 {
 
         Scanner in = new Scanner(System.in);
         int num = Integer.parseInt(in.nextLine());
-        List<String> words = Arrays.asList(in.nextLine().split("\\s+"));
-        //LinkedHashSet<String> words = new LinkedHashSet<>(Arrays.asList(in.nextLine().split("\\s+")));
+        LinkedHashSet<String> words = new LinkedHashSet<>();
+
+        List<String> A = Arrays.asList(in.nextLine().split("\\s+"));
+
+        int i=0;
+        for (String s : A) {
+            if(i<num) words.add(s);
+            i++;
+        }
 
         for (String word : words) {
             HashSet<Character> chars = new HashSet<>();
