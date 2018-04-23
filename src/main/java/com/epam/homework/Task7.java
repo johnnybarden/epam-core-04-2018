b2fc00a2e8beb0d0323ec4d2f6e116dd41ffe195
Dgafar <nithogg@rambler.ru>
2018-04-23 11:18:30 +0300

    fixed

--- src/main/java/com/epam/homework/Task7.java
+++ src/main/java/com/epam/homework/Task7.java
@@ -1,6 +1,7 @@
 package com.epam.homework;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 import static java.lang.String.join;
@@ -35,19 +36,21 @@ public class Task7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.next());
-        HashSet<String> result = new HashSet<>();
+        HashSet<String> resultLowerCase = new LinkedHashSet<>();
+        HashSet<String> result = new LinkedHashSet<>();
         String output = "";
         for (int i = 0; i < n; i++) {
             String lineStart = scanner.next();
             String line = lineStart.toLowerCase();
             int count = 0;
-            if (line.matches("[a-zA-Z]+")) {
+            if (lineStart.matches("[a-zA-Z]+")) {
                 for (int j=0; j<line.length(); j++) {
                     if (line.indexOf(line.charAt(j)) == line.lastIndexOf(line.charAt(j))) count++;
                     else break;
                 }
             }
-            if (count == line.length()) result.add(lineStart);
+            if (count == line.length()) resultLowerCase.add(line);
+            if (resultLowerCase.size()>result.size()) result.add(lineStart);
         }
         if (result.isEmpty()) System.out.println("NOT FOUND");
         else {
