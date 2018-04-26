3beb784a5437004720c00a3e0d5ddee0be07a194
m1nsk <punch.korolev@yandex.ru>
2018-04-27 00:38:37 +0300

    first commit

--- src/main/java/com/epam/homework/Task9.java
+++ src/main/java/com/epam/homework/Task9.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Task9 {
 
     /**
@@ -47,6 +51,37 @@ public class Task9 {
      * NO ROOTS
      */
     public static void main(String[] args) {
-        // TODO реализация
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        try (Scanner reader = new Scanner(System.in)) {
+            double a = reader.nextDouble();
+            double b = reader.nextDouble();
+            double c = reader.nextDouble();
+            List<Double> roots = getRoots(a, b, c);
+            if (roots.size() == 0)
+                System.out.println("NO ROOTS");
+            else {
+                System.out.println(roots.stream().map(item -> formatter.format(item)).collect(Collectors.joining(" ")));
+            }
+        }
+    }
+
+    private static List<Double> getRoots(double a, double b, double c) {
+        double discriminant = getDiscriminant(a, b, c);
+        if (discriminant > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            return Arrays.asList(x1, x2);
+        }
+        else if (discriminant == 0) {
+            double x;
+            x = -b / (2 * a);
+            return Arrays.asList(x);
+        }
+        return new ArrayList<>();
+    }
+
+    private static double getDiscriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
     }
 }
