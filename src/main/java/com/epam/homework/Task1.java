fda30753b1a7dba6328feb8a8c96aa6c8d33185d
Paul <teronozavr@yahoo.com>
2018-04-27 00:19:13 +0300

    Added realisation of searching min and max string.

--- src/main/java/com/epam/homework/Task1.java
+++ src/main/java/com/epam/homework/Task1.java
@@ -1,4 +1,6 @@
-package com.epam.homework;
+package main.java.com.epam.homework;
+
+import java.util.Scanner;
 
 public class Task1 {
 
@@ -7,36 +9,63 @@ public class Task1 {
      * Найти среди них самую короткую и самую длинную.
      * Вывести найденные строки и их длину.
      * Если строк, удовлетворяющих условию, более одной - вывести последнюю из них.
-     *
+     * <p>
      * Формат входных данных:
      * N (целое число, 0 < N < 100) - количество доступных для чтения строк
      * Строка_1
      * Строка_2
      * ...
      * Строка_N
-     *
+     * <p>
      * Формат выходных данных:
      * MIN ($длина_минимальной_строки$): $минимальная_строка$
      * MAX ($длина_максимальной_строки$): $максимальная_строка$
-     *
+     * <p>
      * -----------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 4
      * Унылая пора! Очей очарованье!
      * Приятна мне твоя прощальная краса —
      * Люблю я пышное природы увяданье,
      * В багрец и в золото одетые леса,
-     *
+     * <p>
      * Выходные данные:
      * MIN (29): Унылая пора! Очей очарованье!
      * MAX (35): Приятна мне твоя прощальная краса —
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner = new Scanner(System.in);
 
-        // TODO System.out.println("MIN (" + minLength + "): " + minString);
-        // TODO System.out.println("MAX (" + maxLength + "): " + maxString);
+        String maxString = scanner.nextLine();
+        String minString = maxString;
+
+        for (int i = 0; i <= n - 2; i++) {
+            String tempString = scanner.nextLine();
+
+            maxString = getMaxString(maxString, tempString);
+            minString = getMinString(minString, tempString);
+        }
+
+        System.out.println("MIN (" + minString.length() + "): " + minString);
+        System.out.println("MAX (" + maxString.length() + "): " + maxString);
+    }
+
+    private static String getMinString(String minString, String tempString) {
+        if (tempString.length() <= minString.length()) {
+            minString = tempString;
+        }
+        return minString;
+    }
+
+    private static String getMaxString(String maxString, String tempString) {
+        if (tempString.length() >= maxString.length()) {
+            maxString = tempString;
+        }
+        return maxString;
     }
 }
+
