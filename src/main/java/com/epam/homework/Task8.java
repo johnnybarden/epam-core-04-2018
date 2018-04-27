6e0f4d86f8d2a63e7be045fd8d222a9785f193cc
Stanislav Telnov <stas.telnov89@yandex.ru>
2018-04-27 17:36:57 +0300

    first commit

--- src/main/java/com/epam/homework/Task8.java
+++ src/main/java/com/epam/homework/Task8.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task8 {
 
     /**
@@ -28,6 +30,46 @@ public class Task8 {
      * 22
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner reader = new Scanner(System.in)) {
+            int wordsCount = reader.nextInt();
+            LinkedHashSet<String> uniqueNumPalindromes = new LinkedHashSet<>();
+
+            for (int i = 0; i < wordsCount; i++) {
+                String nextWord = reader.next();
+
+                if (isParsedToInteger(nextWord) && isPalindrome(nextWord)) {
+                    uniqueNumPalindromes.add(nextWord);
+
+                    if (uniqueNumPalindromes.size() > 1) {
+                        break;
+                    }
+                }
+            }
+
+            if (uniqueNumPalindromes.isEmpty()) {
+                System.out.println("NOT FOUND");
+            } else {
+                System.out.println(getLastElement(uniqueNumPalindromes));
+            }
+        }
+    }
+
+    private static <T> T getLastElement(LinkedHashSet<T> set) {
+        return new ArrayList<>(set).get(set.size() - 1);
+    }
+
+
+    private static boolean isParsedToInteger(String str) {
+        return str.matches("\\d+");
+    }
+
+    private static boolean isPalindrome(String str) {
+        for (int i = 0, j = str.length() - 1; i <= j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
