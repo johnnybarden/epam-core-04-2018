620edefc7ad263b29351f3c1060237b2647a886d
Ilya21 <vertileckii.ilya@gmail.com>
2018-05-01 21:43:08 +0300

    Solution of task12

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task12 {
 
     /**
@@ -7,18 +9,18 @@ public class Task12 {
      * Упорядочить строки матрицы в порядке возрастания значений элементов k-го столбца.
      * При совпадении значений элементов - строки матрицы должны сохранить такой же относительный порядок как в исходной матрице.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k < N)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 5
      * 0    2    3    4    5
@@ -27,7 +29,7 @@ public class Task12 {
      * 5    4   -4   -2    2
      * 1    3   -3   -4    3
      * 2
-     *
+     * <p>
      * Выходные данные:
      * 5
      * 7   -1   -5    5    0
@@ -36,7 +38,16 @@ public class Task12 {
      * 1    3    0    2   -1
      * 0    2    3    4    5
      */
+
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int sizeMat = in.nextInt();
+        Matrix myMatrix = new Matrix(sizeMat);
+        myMatrix.setMatrix(in);
+
+        int numberOfColumn = in.nextInt();
+        myMatrix.sortMatrixInTheOrderOfTheKthColumn(numberOfColumn);
+
+        myMatrix.printMatrix();
     }
 }
