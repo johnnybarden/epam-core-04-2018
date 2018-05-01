40a20d5ec257f432d296a92dc8cf758e6af48e03
Alina Putintseva <putintseva.alina@gmail.com>
2018-05-01 16:05:23 +0300

    Task 10 was added

--- src/main/java/com/epam/homework/Task10.java
+++ src/main/java/com/epam/homework/Task10.java
@@ -1,35 +1,49 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task10 {
 
     /**
      * Ввести число с консоли.
      * С помощью битовых, логических и арифметических операций определить, является ли указанное значение степенью двойки.
-     *
+     * <p>
      * Формат входных данных:
      * value - целое число (Integer.MIN_VALUE <= value <= Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * YES - число является степенью двойки
      * NO - число не является степенью двойки
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * -128
-     *
+     * <p>
      * Выходные данные:
      * NO
-     *
-     *
+     * <p>
+     * <p>
      * Входные данные:
      * 256
-     *
+     * <p>
      * Выходные данные:
      * YES
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner in = new Scanner(System.in)) {
+            int value = in.nextInt();
+
+            if (isPowerOfTwo(value)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    static boolean isPowerOfTwo(int value) {
+        return value > 0 && (value & (value - 1)) == 0;
     }
 }
