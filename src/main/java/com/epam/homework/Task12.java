b67051c4d76d82ee1a8718224618a834b26c83c2
goponenkoev <goponenko.yekaterina@yandex.ru>
2018-05-01 19:42:06 +0300

    First commit

--- src/main/java/com/epam/homework/Task12.java
+++ src/main/java/com/epam/homework/Task12.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task12 {
 
     /**
@@ -38,5 +40,35 @@ public class Task12 {
      */
     public static void main(String[] args) {
         // TODO реализация
+        try(Scanner scanner  = new Scanner(System.in)){
+            int[][] matrix = readMatrix(scanner);
+            printMatrix(sortColumn(matrix, scanner.nextInt()));
+        }
+    }
+
+    private static int[][] sortColumn(int[][] matrix, int k) {
+        Arrays.sort(matrix, (o1, o2) -> o1[k] - o2[k]);
+        return matrix;
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        System.out.println(matrix.length);
+        for (int[] aMatrix : matrix) {
+            for (int col = 0; col < matrix.length; ++col) {
+                System.out.print(aMatrix[col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
     }
 }
