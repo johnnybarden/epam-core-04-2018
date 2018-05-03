53a553656b7de70357f9d6d53c881b6978d4df24
Ilya21 <vertileckii.ilya@gmail.com>
2018-05-04 02:42:34 +0300

    overwritten class matrix with ArrayList

---
+++ src/main/java/com/epam/homework/Matrix.java
@@ -0,0 +1,59 @@
+package com.epam.homework;
+
+import java.util.*;
+
+class Matrix {
+
+    private ArrayList<Integer[]> matrix;
+    private int matrixSize;
+
+    Matrix(int matrixSize) {
+        this.matrix = new ArrayList<>();
+        this.matrixSize = matrixSize;
+    }
+
+    void setMatrix(Scanner in) {
+        for (int i = 0; i < matrixSize; i++) {
+            Integer[] temp = new Integer[matrixSize];
+            for (int j = 0; j < matrixSize; j++) {
+                temp[j] = in.nextInt();
+            }
+            matrix.add(temp);
+        }
+    }
+
+    void printMatrix() {
+        System.out.println(matrixSize);
+        for (Integer[] row : matrix) {
+            for (Integer element : row) {
+                System.out.printf("%5d ", element);
+            }
+            System.out.println();
+        }
+    }
+
+    void sortMatrixInTheOrderOfTheKthColumn(int numbOfCol) {
+        matrix.sort(new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                return Integer.compare(o1[numbOfCol], o2[numbOfCol]);
+            }
+        });
+    }
+
+    void shiftRows(int amountShifts) {
+        amountShifts %= matrixSize;
+        if (amountShifts > 0) {
+            shift(matrixSize - amountShifts);
+        } else {
+            shift(Math.abs(amountShifts));
+        }
+    }
+
+    private void shift(int amountShifts) {
+        for (int i = 0; i < amountShifts; i++) {
+            matrix.add(matrix.get(0));
+            matrix.remove(0);
+        }
+    }
+}
--- src/main/java/com/epam/homework/Task13.java
+++ src/main/java/com/epam/homework/Task13.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.*;
+
 public class Task13 {
 
     /**
@@ -9,40 +11,40 @@ public class Task13 {
      * k < 0 - сдвиг матрицы вверх
      * k = 0 - матрица остается без изменений
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
      * k - целое число (0 <= k <= 100)
-     *
+     * <p>
      * Формат выходных данных:
      * Матрица после выполнения преобразования
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Примеры выполнения задания:
-     *
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * -2
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 0  -1   2
      * 4   2   3
      * 1   0  -3
-     *
-     *
-     *
+     * <p>
+     * <p>
+     * <p>
      * Входные данные:
      * 3
      * 4   2   3
      * 1   0  -3
      * 0  -1   2
      * 0
-     *
+     * <p>
      * Выходные данные:
      * 3
      * 4   2   3
@@ -50,6 +52,15 @@ public class Task13 {
      * 0  -1   2
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner in = new Scanner(System.in);
+        int sizeOfMat = in.nextInt();
+        Matrix myMat = new Matrix(sizeOfMat);
+        myMat.setMatrix(in);
+
+        int amountShifts = in.nextInt();
+        myMat.shiftRows(amountShifts);
+
+        myMat.printMatrix();
+
     }
 }
