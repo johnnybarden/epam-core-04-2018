f0a26810b3c6374478159e79ed2f33883bb508b2
EugeneGrim <eugene.grim@gmail.com>
2018-05-04 19:46:14 +0300

    Task9: complete

--- src/main/java/com/epam/homework/Task9.java
+++ src/main/java/com/epam/homework/Task9.java
@@ -1,5 +1,12 @@
 package com.epam.homework;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Task9 {
 
     /**
@@ -46,7 +53,26 @@ public class Task9 {
      * Выходные данные:
      * NO ROOTS
      */
-    public static void main(String[] args) {
-        // TODO реализация
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] factors = reader.readLine().split(" ");
+        int a = Integer.parseInt(factors[0]);
+        int b = Integer.parseInt(factors[1]);
+        int c = Integer.parseInt(factors[2]);
+
+        double d = b*b - 4.0*a*c;
+
+        double x1, x2;
+
+        if (d > 0) {
+            x1 = (-b + Math.sqrt(d)) / 2.0*a;
+            x2 = (-b - Math.sqrt(d)) / 2.0*a;
+            System.out.print(String.format(Locale.ENGLISH, "%.2f, %.2f%n", x1, x2));
+        } else if (d == 0) {
+            x1 = -b / (2.0*a);
+            System.out.print(String.format(Locale.ENGLISH, "%.2f", x1));
+        } else {
+            System.out.println("NO ROOTS");
+        }
     }
 }
