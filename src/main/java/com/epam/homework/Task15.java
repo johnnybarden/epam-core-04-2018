fc7a74e621f664b388fce66a69a444546e822133
LicenserX <your@mail.here>
2018-05-20 21:06:05 +0300

    Таска 15.

--- src/main/java/com/epam/homework/Task15.java
+++ src/main/java/com/epam/homework/Task15.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task15 {
 
     /**
@@ -7,27 +9,61 @@ public class Task15 {
      * В случае, если в строке нет двух положительных элементов - сумма от этой строки полагается равной нулю.
      * Сумма между двумя рядом расположенными элементами также равна нулю.
      * <a href="https://github.com/elefus/epam-core-04-2018/wiki/Представление-матриц">Представление матриц</a>
-     *
+     * <p>
      * Формат входных данных:
      * N - целое число (0 < N < 10)
      * N ^ 2 целых чисел (Integer.MIN_VALUE < element < Integer.MAX_VALUE)
-     *
+     * <p>
      * Формат выходных данных:
      * Целое число, представляющее вычисленную сумму
-     *
+     * <p>
      * ---------------------------------------------------------------------------------------------------
      * Пример выполнения задания:
-     *
+     * <p>
      * Входные данные:
-     *  3
-     *  1   0   3
-     * -1   2   2
-     *  1  -1   3
-     *
+     * 3
+     * 1   0   3
+     * -1  2   2
+     * 1  -1   3
+     * <p>
      * Выходные данные:
      * -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = readMatrix(scanner);
+
+        System.out.println(getSumBetweenFirstAndSecondPositiveValues(matrix));
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static int getSumBetweenFirstAndSecondPositiveValues(int[][] matrix) {
+        int startIndex = 0;
+        int sum = 0;
+
+        for (int[] matrixRow : matrix) {
+            for (int i = 0; i < matrixRow.length; i++) {
+                if (matrixRow[i] > 0) {
+                    startIndex = i;
+                    break;
+                }
+            }
+            for (int j = startIndex; j < matrixRow.length; j++) {
+                if (matrixRow[j] <= 0) {
+                    sum += matrixRow[j];
+                }
+            }
+        }
+        return sum;
     }
 }
