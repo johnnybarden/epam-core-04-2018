5fbfeb25aa2c7552d1a34a59b312111ad5ba994d
johnnybarden <johnnybarden@yandex.ru>
2018-06-15 00:08:06 +0300

    Task 19 completed.

--- src/main/java/com/epam/homework/Task19.java
+++ src/main/java/com/epam/homework/Task19.java
@@ -1,5 +1,9 @@
 package com.epam.homework;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Task19 {
 
     /**
@@ -36,6 +40,74 @@ public class Task19 {
      * -3  1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner sc = new Scanner(System.in)) {
+            int[][] initialMatrix = readMatrix(sc);
+            int[][] matrixWithoutZeros = removeAllZerosRowsAndCols(initialMatrix);
+            System.out.println(matrixWithoutZeros.length);
+            System.out.println(matrixWithoutZeros[0].length);
+            printMatrix(matrixWithoutZeros);
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+
+    private static int[][] removeAllZerosRowsAndCols(int[][] matrix) {
+        Set<Integer> rowsToDelete = new HashSet<>();
+        Set<Integer> colsToDelete = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] != 0) {
+                    break;
+                }
+                if (j == matrix.length - 1) {
+                    rowsToDelete.add(i);
+                }
+            }
+            for (int k = 0; k < matrix.length; k++) {
+                if (matrix[k][i] != 0) {
+                    break;
+                }
+                if (k == matrix.length - 1) {
+                    colsToDelete.add(i);
+                }
+            }
+        }
+        int[][] newMatrix = new int[matrix.length - rowsToDelete.size()][matrix.length - colsToDelete.size()];
+
+        int rowsShift = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            if (rowsToDelete.contains(i)) {
+                rowsShift++;
+            } else {
+                int colsShift = 0;
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (colsToDelete.contains(j)) {
+                        colsShift++;
+                    } else {
+                        newMatrix[i - rowsShift][j - colsShift] = matrix[i][j];
+                    }
+                }
+            }
+        }
+        return newMatrix;
+    }
+
+    private static void printMatrix(int[][]matrix) {
+        for (int[] aMatrix : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(aMatrix[j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
