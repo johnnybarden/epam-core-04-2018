9e04f7eb423add06b5f4ad0cecd289cd855f7e0a
johnnybarden <johnnybarden@yandex.ru>
2018-06-16 21:24:29 +0300

    Task 21 in progress.

---
+++ src/main/java/com/epam/homework/task21/Task21Realisation.java
@@ -0,0 +1,17 @@
+package com.epam.homework.task21;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Task21Realisation implements Task21 {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public List<String> reverseFile(File input, File output) throws IOException {
+        return null;
+    }
+}
