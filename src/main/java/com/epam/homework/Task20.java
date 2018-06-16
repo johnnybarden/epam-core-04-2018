4ee626d9a8859168ca2a798e99c40ba8afacfcd5
johnnybarden <johnnybarden@yandex.ru>
2018-06-16 15:34:10 +0300

    Task 20 completed.

--- src/main/java/com/epam/homework/Task20.java
+++ src/main/java/com/epam/homework/Task20.java
@@ -1,5 +1,7 @@
 package com.epam.homework;
 
+import java.util.Scanner;
+
 public class Task20 {
 
     /**
@@ -36,6 +38,60 @@ public class Task20 {
      * -2  1  4 -1
      */
     public static void main(String[] args) {
-        // TODO реализация
+        try (Scanner sc = new Scanner(System.in)) {
+            int rowToInsertMin = Integer.parseInt(sc.nextLine());
+            int colToInsertMin = Integer.parseInt(sc.nextLine());
+            int[][] initialMatrix = readMatrix(sc);
+            int[][] matrixWithMinReplaced = getAndMoveMinElement(rowToInsertMin, colToInsertMin, initialMatrix);
+            System.out.println(matrixWithMinReplaced.length);
+            printMatrix(matrixWithMinReplaced);
+        }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        int dimension = scanner.nextInt();
+        int[][] matrix = new int[dimension][dimension];
+        for (int row = 0; row < dimension; ++row) {
+            for (int col = 0; col < dimension; ++col) {
+                matrix[row][col] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private static int[][] getAndMoveMinElement(int targetRow, int targetCol, int[][] matrix) {
+        int minElement = Integer.MAX_VALUE;
+        int rowContainingMin = 0;
+        int colContainingMin = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] < minElement) {
+                    minElement = matrix[i][j];
+                    rowContainingMin = i;
+                    colContainingMin = j;
+                }
+            }
+        }
+
+        int[] tempRow = matrix[rowContainingMin];
+        matrix[rowContainingMin] = matrix[targetRow];
+        matrix[targetRow] = tempRow;
+
+        for (int[] row : matrix) {
+            int tmp = row[colContainingMin];
+            row[colContainingMin] = row[targetCol];
+            row[targetCol] = tmp;
+            }
+
+        return matrix;
+    }
+
+    private static void printMatrix(int[][]matrix) {
+        for (int[] aMatrix : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(aMatrix[j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
