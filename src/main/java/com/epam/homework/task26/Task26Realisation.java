d9c42dd7463ffbe1ef2e4f062cb6f97ae2612954
johnnybarden <johnnybarden@yandex.ru>
2018-06-20 01:33:35 +0300

    Task 26 in progress.

---
+++ src/main/java/com/epam/homework/task26/Task26Realisation.java
@@ -0,0 +1,10 @@
+package com.epam.homework.task26;
+
+import java.util.Set;
+
+public class Task26Realisation implements Task26 {
+    @Override
+    public Set<I2DPoint> analyze(Set<ISegment> segments) {
+        return null;
+    }
+}
